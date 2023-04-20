e1eb245e69ca16292ae23377521afc68b5741689
kimsunghyun <berserk.gahz@gmail.com>
2023-04-20 16:20:12 +0900

    join

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,20 @@
 const express = require("express");
 const router = express.Router();
-
+const bcrypt = require("bcrypt");
+router.post("/join", async (req, res, next) => {
+  const nick = req.body.nick;
+  const email = req.body.email;
+  const password = req.body.password;
+  const exUser = await User.findOne({ where: { email } });
+  if (exUser) {
+    return res.redirect("/join/?error=conflict");
+  } else {
+    const hash = await bcrypt.hash(password, 12);
+    await User.create({
+      nick,
+      email,
+      password: hash,
+    });
+  }
+});
 module.exports = router;
