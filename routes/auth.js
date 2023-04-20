aaa3bbd0aaff32887de67cf5d877a37badddc5db
kimsunghyun <berserk.gahz@gmail.com>
2023-04-20 16:43:23 +0900

    'logout'

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const passport = require("passport");
 router.post("/join", async (req, res, next) => {
   const nick = req.body.nick;
   const email = req.body.email;
@@ -24,6 +25,25 @@ router.get("/logout", (req, res, next) => {
     }
     req.session.destroy();
     return res.redirect("/");
+  });
+});
+
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (authError, user, info) => {
+    if (authError) {
+      console.error(authError);
+      return next(authError);
+    }
+    if (!user) {
+      return res.redirect(`/?loginError=${info.message}`);
+    }
+    return req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        return next(loginError);
+      }
+      return res.redirect("/");
+    });
   });
 });
 module.exports = router;
